15007481d08b360c3e01ca5f9bbc257d111f6764
kutmanhare <kutmankojonov54@gmail.com>
2025-03-15 16:01:20 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [results, Setresults] = useState(false)
+	const [photos, Setphotos] = useState([])
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/photos?_limit=9')
+			.then((res) => res.json())
+			.then(
+				(result) => {
+					Setresults(true)
+					Setphotos(result)
+				},
+				(error) => {
+					Setresults(false)
+					console.log(error)
+				},
+			)
+	}, [])
+	return (
+		<div className='App'>
+			{results &&
+				photos.map((photo) => {
+					return (
+						<div className='photo-card' key={photo.id}>
+							<h4>{photo.title}</h4>,
+							<img
+								src={photo.thumbnailUrl}
+								alt=''
+							></img>
+							<p className='photo-title'>{photo.id}</p>
+						</div>
+					)
+				})}
+		</div>
+	)
 }
 
-export default App;
+export default App
